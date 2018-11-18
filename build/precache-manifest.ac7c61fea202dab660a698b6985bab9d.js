37c0ce81daf223d6775ad93200c48840bf7ad398
fichorka <fichorka@gmail.com>
2018-11-18 17:42:02 +0100

    Add build folder

---
+++ build/precache-manifest.ac7c61fea202dab660a698b6985bab9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "320d0aa46d7edc27058d818448b70d23",
+    "url": "/static/media/about1.320d0aa4.jpeg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5ed469b3794241c75908",
+    "url": "/static/js/main.5ed469b3.chunk.js"
+  },
+  {
+    "revision": "2ab7f6a4851c0658a93d",
+    "url": "/static/js/1.2ab7f6a4.chunk.js"
+  },
+  {
+    "revision": "5ed469b3794241c75908",
+    "url": "/static/css/main.7b641759.chunk.css"
+  },
+  {
+    "revision": "884d453a3293aa50637b9c83c866539e",
+    "url": "/index.html"
+  }
+];
